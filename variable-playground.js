a059fa91e676c3ab28e224291aef55abda55da4d
timetobehappy <jibiu@outlook.com>
2016-03-09 10:03:00 -0600

    Add varable playground area

---
+++ variable-playground.js
@@ -0,0 +1,36 @@
+// var person = {
+// 	name: 'Andrew',
+// 	age:21
+// };
+
+
+// function updatePerson(obj){
+// 	// obj = {
+// 	// 	name: 'jose',
+// 	// 	age: 33
+// 	// };
+
+// 	obj.age = 44;
+
+// 	console.log(obj);
+// }
+
+
+// updatePerson(person);
+
+// console.log(person);
+
+
+//Array Example
+
+
+var grades = [15,88];
+
+function addGrades (gradesArr) {
+	gradesArr.push(55);
+	debugger;
+}
+
+
+addGrades(grades);
+console.log(grades);

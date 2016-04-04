f8316e71093e67548a11f21566b37dda12ae496a
timetobehappy <jibiu@outlook.com>
2016-04-04 15:55:03 -0500

    Convert email to lowercase

--- models/user.js
+++ models/user.js
@@ -1,4 +1,4 @@
-module.exports = function (sequelize, DataTypes){
+module.exports = function(sequelize, DataTypes) {
 	return sequelize.define('user', {
 		email: {
 			type: DataTypes.STRING,
@@ -12,9 +12,18 @@ module.exports = function (sequelize, DataTypes){
 			type: DataTypes.STRING,
 			allowNull: false,
 			validate: {
-				len: [7,100]
+				len: [7, 100]
+			}
+		}
+
+	}, {
+		hooks: {
+			beforeValidate: function(user, options) {
+				if (typeof user.email === 'string') {
+					user.email = user.email.toLowerCase();
+				}
 			}
 
 		}
-	})
-}
+	});
+};
